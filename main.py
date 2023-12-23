cd623d676d906c8da3d48da05d79078472d8e306
Opyau <opyau@ucdavis.edu>
2023-12-22 18:05:27 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from matplotlib import rcParams
+import matplotlib.pyplot as plt
+import numpy as np
+import keras
