9923c802341fe8ee89b924e215af7f03fa3e1f44
Oliver <opyau@yahoo.com>
2023-12-22 20:06:09 -0800

    Constructed Binary Classication Model

---
+++ classification.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from matplotlib import rcParams
+import matplotlib.pyplot as plt
+import numpy as np
+import keras
+
+
+def Logistic_Regression(x_train, y_train, x_test, y_test):
+    tf.random.set_seed(100)
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(16, activation='relu'),
+        tf.keras.layers.Dense(32, activation='relu'),
+        tf.keras.layers.Dense(32, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.03),
+                  loss='binary_crossentropy',
+                  metrics=[
+                        tf.keras.metrics.BinaryAccuracy(name='accuracy'),
+                        tf.keras.metrics.Precision(name='precision'),
+                        tf.keras.metrics.Recall(name='recall')
+                    ]
+                )
+    model.fit(x_train, y_train, epochs=5)
+
+    loss_and_metrics = model.evaluate(x_test, y_test)
+    print(loss_and_metrics)
+    print('Loss = ', loss_and_metrics[0])
+    print('Accuracy = ', loss_and_metrics[1])
+    model.save('Oliver Model.keras')
+
--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from Clean import *
+from classification import *
+
+
 
 if __name__ == '__main__':
-    clean()
+    # clean()
+    #
+    # x_train = pd.read_csv('scaled_train.csv').iloc[:, 1:]
+    # x_test = pd.read_csv('scaled_test.csv').iloc[:, 1:]
+    #
+    # y_train = pd.read_csv('train_labels.csv').iloc[:, 1:]
+    # y_test = pd.read_csv('test_labels.csv').iloc[:, 1:]
+    #
+    # prediction = Logistic_Regression(x_train, y_train, x_test, y_test)
